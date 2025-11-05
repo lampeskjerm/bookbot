af2e96d422738575881ae75d335d4cd48d6e4e90
lampeskjerm <slkajmeprem@gmail.com>
2025-11-05 22:10:40 +0100

    made it work with any book

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import sys
+if len(sys.argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 from stats import get_num_words
-from stats import characters
 from stats import sort_characters
 
 def main():
-    result = characters("books/frankenstein.txt")
-    sorted_list = sort_characters("books/frankenstein.txt")
+    sorted_list = sort_characters(sys.argv[1])
     
-    print("============ BOOKBOT ===========\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
-    print(f"Found {get_num_words("books/frankenstein.txt")} total words")
+    print(f"============ BOOKBOT ===========\nAnalyzing book found at {sys.argv[1]}...\n----------- Word Count ----------")
+    print(f"Found {get_num_words(sys.argv[1])} total words")
     print("--------- Character Count -------")
     for item in sorted_list:
         print(item["char"] + ": " + str(item["num"]))
